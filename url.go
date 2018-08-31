6f599e56b3eb2f6dbbddff4a7fb45024bcace74d
tike <timflex@gmx.de>
2018-08-31 17:31:41 +0200

    url type

---
+++ url.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"fmt"
+	"net/url"
+)
+
+// URL allows to specify URLs in config files parsing directly to the native type.
+type URL struct {
+	*url.URL
+}
+
+// UnmarshalTOML implements toml.Unmarshaller.
+func (u *URL) UnmarshalTOML(v interface{}) (err error) {
+	s, ok := v.(string)
+	if !ok {
+		return fmt.Errorf("config.URL: can't unmarshal non string value (%T) into URL", v)
+	}
+
+	u.URL, err = url.Parse(s)
+	return
+}
